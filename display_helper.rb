e2df5009711dc0d33c3c8ef5a5d9d75ffe3f6d63
OJ De Quintos <2008oliverjesus@gmail.com>
2023-05-13 18:46:19 +0800

    Update display using \n and \t

--- display_helper.rb
+++ display_helper.rb
@@ -6,15 +6,15 @@ module DisplayHelper
   def print_conversion(b, b2, multiplicator_1, multiplicator_2, input, format_code, old_input)
     if b == b2
       offered_bundle_by_order(input, old_input)
-      puts "#{input} #{format_code.upcase} $#{grand_total_one_bundle_per_order(format_code, b)}
-            #{multiplicator_1 + multiplicator_2} x #{b} $#{
+      puts "#{input} #{format_code.upcase} $#{grand_total_one_bundle_per_order(format_code, b)}\n\t#{
+              multiplicator_1 + multiplicator_2} x #{b} $#{
               compute_order_total_by_bundle(format_code, b, (multiplicator_1 + multiplicator_2))
             }" and return
     else
       offered_bundle_by_order(input, old_input)
-      puts "#{input} #{format_code.upcase} $#{grand_total_multiple_bundle_per_order(format_code, b, b2, multiplicator_1, multiplicator_2)}
-            #{multiplicator_1} x #{b} $#{(compute_order_total_by_bundle(format_code, b, multiplicator_1))}
-            #{multiplicator_2} x #{b2} $#{(compute_order_total_by_bundle(format_code, b2, multiplicator_2))}" and return
+      puts "#{input} #{format_code.upcase} $#{grand_total_multiple_bundle_per_order(format_code, b, b2, multiplicator_1, multiplicator_2)}\n\t#{
+        multiplicator_1} x #{b} $#{(compute_order_total_by_bundle(format_code, b, multiplicator_1))}\n\t#{
+        multiplicator_2} x #{b2} $#{(compute_order_total_by_bundle(format_code, b2, multiplicator_2))}" and return
     end
   end
 
